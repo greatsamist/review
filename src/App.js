60565542539a8163fc376d40d882c7348943c436
Samuel Ojo <ifegbenga@gmail.com>
2022-02-14 19:40:00 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import Styles from "./App.module.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.header}>
+        <h1 className={Styles["header__heading"]}>Our Reviews</h1>
+        <div className={Styles["header__underline"]}></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
