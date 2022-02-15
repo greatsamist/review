416578306e6a1f7335eeffc643168d3f50d5b327
Samuel Ojo <ifegbenga@gmail.com>
2022-02-15 23:50:03 +0100

    changes

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 import data from "./Reviews";
 import Styles from "./Review.module.scss";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 function Review() {
-  const [reviews, setReviews] = useState(data);
+  const [reviews, setReviews] = useState(0);
+  const { id, image, job, name, text } = data[reviews];
 
   return (
     <article>
-      {reviews.map((review) => {
-        const { id, image, job, name, text } = review;
-        return (
-          <div className={Styles.review} key={id}>
-            <div className={Styles["review__imgContain"]}>
-              <img className={Styles["review__img"]} src={image} alt="" />
-            </div>
-            <h3 className={Styles["review__name"]}>{name}</h3>
-            <p className={Styles["review__job"]}>{job}</p>
-            <p className={Styles["review__text"]}>{text}</p>
-          </div>
-        );
-      })}
+      <div className={Styles.review} key={id}>
+        <div className={Styles["review__imgContain"]}>
+          <img className={Styles["review__img"]} src={image} alt="" />
+          <span className={Styles["quote-icon"]}>
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h3 className={Styles["review__name"]}>{name}</h3>
+        <p className={Styles["review__job"]}>{job}</p>
+        <p className={Styles["review__text"]}>{text}</p>
+        <div className={Styles["review__btnContain"]}>
+          <button className={Styles["review__prevBtn"]}>
+            <FaChevronLeft />
+          </button>
+          <button className={Styles["review__nextBtn"]}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className={Styles["review__surprise"]}>Surprise me</button>
+      </div>
     </article>
   );
 }
