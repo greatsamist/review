432cc4bc4dda3fdce090e550ff1e5359f1cabec8
Samuel Ojo <ifegbenga@gmail.com>
2022-02-17 17:04:34 +0100

    changes

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -5,11 +5,36 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 function Review() {
   const [reviews, setReviews] = useState(0);
-  const { id, image, job, name, text } = data[reviews];
+  const { image, job, name, text } = data[reviews];
+  console.log(data.length - 1);
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    } else if (number < 0) {
+      return data.length + 1;
+    } else {
+      return 2;
+    }
+  };
+
+  const prevPersonHandler = () => {
+    setReviews((reviews) => {
+      let newReview = reviews - 1;
+      return checkNumber(newReview);
+    });
+  };
+  const nextPersonHandler = () => {
+    setReviews((reviews) => {
+      let newReview = reviews + 1;
+      return checkNumber(newReview);
+    });
+  };
+
+  const surpriseMeHandler = () => {};
 
   return (
     <article>
-      <div className={Styles.review} key={id}>
+      <div className={Styles.review}>
         <div className={Styles["review__imgContain"]}>
           <img className={Styles["review__img"]} src={image} alt="" />
           <span className={Styles["quote-icon"]}>
@@ -20,17 +45,27 @@ function Review() {
         <p className={Styles["review__job"]}>{job}</p>
         <p className={Styles["review__text"]}>{text}</p>
         <div className={Styles["review__btnContain"]}>
-          <button className={Styles["review__prevBtn"]}>
+          <button
+            className={Styles["review__prevBtn"]}
+            onClick={prevPersonHandler}
+          >
             <FaChevronLeft />
           </button>
-          <button className={Styles["review__nextBtn"]}>
+          <button
+            className={Styles["review__nextBtn"]}
+            onClick={nextPersonHandler}
+          >
             <FaChevronRight />
           </button>
         </div>
-        <button className={Styles["review__surprise"]}>Surprise me</button>
+        <button
+          className={Styles["review__surprise"]}
+          onClick={surpriseMeHandler}
+        >
+          Surprise me
+        </button>
       </div>
     </article>
   );
 }
-
 export default Review;
